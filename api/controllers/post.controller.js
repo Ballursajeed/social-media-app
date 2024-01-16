2df8e1075f7ca1fa874c7a41f9ea9b72328379d5
Ballursajeed <ballursajeed@gmail.com>
2024-01-16 23:40:37 +0530

    Delele post controller '/delete' enpoint created

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -33,6 +33,50 @@ exports.createPost = async(req,res) => {
        })
    }
 }
+
+ exports.deletePost = async(req,res) => {
+     try {
+
+         const post = await Post.findById(req.params.id);
+
+         if (!post) {
+            return res
+                     .status(404)
+                     .json({
+                        success:false,
+                        message:"Post not found",
+                     });
+       }
+
+       if (post.owner.toString() !== req.user._id.toString()) {
+            return res
+                     .status(401)
+                     .json({
+                             success:false,
+                             message:"UnAuthorized"
+                      })
+       }
+
+       await Post.findByIdAndDelete(req.params.id).populate('owner');
+
+       const user = await User.findById(req.user._id);
+       const index = user.posts.indexOf(req.params.id);
+       user.posts.splice(index,1);
+       await user.save();
+
+       res.status(200).json({
+             success:true,
+             message:"Post deleted"
+       })
+
+     } catch (error) {
+           res.status(500).json({
+           	   success:false,
+               message:error.message
+           })
+     }
+ }
+
 exports.likeAndUnlikePost = async (req,res) => {
        try {
 
@@ -81,4 +125,5 @@ exports.likeAndUnlikePost = async (req,res) => {
              })
        }
 }
+
 
