2df9ff201981d00ad3e883f7fb5ea6bd2a34f66e
Ballursajeed <ballursajeed@gmail.com>
2024-01-16 23:20:02 +0530

    Added like and unlike controller endpoint

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,7 +1,7 @@
 const Post = require("../models/post.model.js")
 const User = require('../models/user.model.js')
 
-const createPost = async(req,res) => {
+exports.createPost = async(req,res) => {
    try {
 
     const newPostData = {
@@ -33,5 +33,52 @@ const createPost = async(req,res) => {
        })
    }
 }
+exports.likeAndUnlikePost = async (req,res) => {
+       try {
 
-module.exports = createPost
+       const post = await Post.findById(req.params.id);
+
+       if (!post) {
+            return res
+                     .status(404)
+                     .json({
+                        success:false,
+                        message:"Post not found",
+                     });
+       }
+
+      if (post.likes.includes(req.user._id)) {
+         const index = post.likes.indexOf(req.user._id);
+
+         post.likes.splice(index,1);
+
+         await post.save();
+
+         return res
+          .status(200)
+          .json({
+           success:true,
+           message:"Post Unliked "
+         })
+      }
+         else {
+              post.likes.push(req.user._id);
+              await post.save()  ;
+        return res
+          .status(200)
+          .json({
+           success:true,
+           message:"Post Liked "
+         })
+         }
+
+       } catch (error) {
+            res
+             .status(500)
+             .json({
+                 success:false,
+                 message:error.message
+             })
+       }
+}
+
