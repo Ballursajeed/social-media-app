19c8dd64cc8752526fdc2565718c91f300535197
Ballursajeed <ballursajeed@gmail.com>
2024-01-19 20:41:54 +0530

    created Login page and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Header from "./component/Header/Header"
+import Login from "./component/Login/Login"
+import { useEffect } from "react";
+import { loadUser } from "./Actions/User"
+import { useDispatch } from "react-redux";
 
 function App() {
+
+ const dispatch = useDispatch();
+
+ useEffect(() => {
+      dispatch(loadUser());
+ },[])
+
   return (
   <Router>
     <Header />
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
   </Router>
   );
 }
