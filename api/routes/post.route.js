bd24374892b9de9ca32197c559159e0fccf31173
Ballursajeed <ballursajeed@gmail.com>
2024-01-19 10:04:18 +0530

    owner of the post can delete any comment

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,7 +4,8 @@ const  { createPost,
          deletePost,
          getPostOfFollowing,
          updatePost,
-         addComment } = require('../controllers/post.controller.js');
+         addComment,
+         deleteComment } = require('../controllers/post.controller.js');
 const { isAuthenticated } = require('../middlewares/auth.js')
 
 const router = express.Router();
@@ -14,6 +15,6 @@ const router = express.Router();
  router.route('/delete/:id').delete(isAuthenticated,deletePost);
  router.route('/update/:id').put(isAuthenticated,updatePost);
  router.route('/').get(isAuthenticated,getPostOfFollowing);
- router.route('/comment/:id').put(isAuthenticated,addComment);
+ router.route('/comment/:id').put(isAuthenticated,addComment).delete(isAuthenticated,deleteComment)
 
 module.exports = router;
