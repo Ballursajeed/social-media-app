e7bddbe62953c50f9f3ae725c3b7fd9b21724cdb
Ballursajeed <ballursajeed@gmail.com>
2024-01-19 15:41:16 +0530

    starting with react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+  <Router>
+    <div className="App">
+     hello from App.js
+    </div>
+  </Router>
+  );
+}
+
+export default App;
