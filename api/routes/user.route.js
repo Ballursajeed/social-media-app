5595e76dcffc4f34fcdcbfbc1ad567bcc13ac580
Ballursajeed <ballursajeed@gmail.com>
2024-01-22 12:53:51 +0530

    added getMyPosts route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -10,7 +10,8 @@ const { Register,
         getUserProfile,
         getAllUsers,
         forgetPassword,
-        resetPassword } = require('../controllers/user.controller.js')
+        resetPassword,
+        getMyPosts } = require('../controllers/user.controller.js')
 const { isAuthenticated } = require('../middlewares/auth.js')
 
 const router = express.Router();
@@ -22,6 +23,7 @@ const router = express.Router();
  router.route('/update/password').put(isAuthenticated,updatePassword);
  router.route('/update/profile').put(isAuthenticated,updateProfile);
  router.route('/me').get(isAuthenticated,myProfile);
+ router.route('/my/posts').get(isAuthenticated,getMyPosts);
  router.route('/delete/me').delete(isAuthenticated,deleteMyProfile);
  router.route('/user/:id').get(isAuthenticated,getUserProfile);
  router.route('/users').get(isAuthenticated,getAllUsers);
