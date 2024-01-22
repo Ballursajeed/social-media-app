359803b1b1bbd1b730d5ccec7ec1c518b39b978e
Ballursajeed <ballursajeed@gmail.com>
2024-01-22 17:07:55 +0530

    created Profile UI

--- client/src/component/Account/Account.js
+++ client/src/component/Account/Account.js
@@ -4,14 +4,16 @@ import { useDispatch,useSelector } from "react-redux";
 import { getMyPosts } from "../../Actions/User";
 import Loader from "../Loader/Loader"
 import Post from "../Post/Post";
-import { Typography }  from "@mui/material";
+import { Typography, Avatar, Button }  from "@mui/material";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Link } from "react-router-dom";
 
 const Account = () => {
 
  const dispatch = useDispatch();
 
+ const { user,loading:userLoading } = useSelector((state) => state.user);
  const { loading, error, posts } = useSelector((state) => state.myPosts);
  const {error:likeError,message} = useSelector((state) => state.like);
 
@@ -47,8 +49,7 @@ const Account = () => {
  return(
   <>
   <ToastContainer />
-     {
-      loading ? <Loader /> :
+     { loading === true || userLoading === true ?  <Loader /> :
       <div className='account'>
         <div className='accountleft'>
              {
@@ -64,11 +65,40 @@ const Account = () => {
                       ownerName = {post.owner.name}
                       ownerId = {post.owner._id}
                   />
-                )) : <Typography variant="h6">No Post Yet</Typography>
+                )) : <Typography variant="h6">You Have not made any Post</Typography>
 
              }
         </div>
         <div className='accountright'>
+           <Avatar src={user.avatar.url}
+                  sx={{ height: "8vmax", width: "8vmax" }} />
+           <Typography variant="h5">{user.name}</Typography>
+           <div>
+             <button>
+              <Typography>Followers</Typography>
+             </button>
+               <Typography>{user.followers.length}</Typography>
+           </div>
+           <div>
+             <button>
+              <Typography>Following</Typography>
+             </button>
+               <Typography>{user.following.length}</Typography>
+           </div>
+           <div>
+              <Typography>Post</Typography>
+              <Typography>{user.posts.length}</Typography>
+            </div>
+            <Button variant="contained">Logout</Button>
+
+            <Link to='/update/profile'>Edit Profile</Link>
+            <Link to='/update/password'>Change Password</Link>
+
+            <Button
+                 variant="text"
+                 style={{ color: "red", margin: "2vmax" }}    >
+               Delete My Profile
+            </Button>
 
         </div>
       </div>
